03bea190731dbd378f6fa62dbf3eae8df394835c
jkkitti <j.kittiwiriya@gmail.com>
2026-03-13 22:31:24 +0700

    edit 06 file

--- 06_bring-it-to-javascript.js
+++ 06_bring-it-to-javascript.js
@@ -1,33 +1,32 @@
-//Create a product list.
-const products = [
-  {
-    id: "P001",
-    name: "Coat hanger",
-    price: 20,
-    stock: 10
-  },
-  {
-    id: "P002",
-    name: "Clothespin",
-    price: 10,
-    stock: 20
-  },
-  {
-    id: "P003",
-    name: "Bowl",
-    price: 40,
-    stock: 15
-  }
-];
+//Categories
+const category = {
+  category_id: 1,
+  category_name: "Beverage"
+};
 
-//View all products function.
-function viewProducts() {
-  console.log("Product List:");
-  products.forEach(product => {
-    console.log(product);
-  });
-}
+//Products
+const product = {
+  product_id: 101,
+  product_name: "Black Coffee",
+  sku_code: "COFFEE-001",
+  price: 50.00,
+  qty_in_stock: 20,
+  category_id: 1 
+};
 
-viewProducts();
+//Users
+const user = {
+  user_id: 9,
+  name: "Aod",
+  role: "Admin"
+};
 
-
+//Stock_Movements
+const movement = {
+  movement_id: 5001,
+  product_id: 101,  
+  user_id: 9,       
+  type: "IN",       
+  quantity: 10,     
+  date: "2025-03-11"
+};
